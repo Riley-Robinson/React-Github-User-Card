4c23f6770443659b704ba2bdc9a44cc6b1d7a6ab
Riley-Robinson <rrtoinfinity@gmail.com>
2020-04-07 18:59:02 -0400

    mvp

--- my-app/src/components/User.js
+++ my-app/src/components/User.js
@@ -4,7 +4,69 @@ import UserCard from "./UserCard";
 import "../App.css";
 
 
+class User extends React.Component{
+        constructor() {
+            super();
+            this.state = {
+                user: {},
+                followers: []
+            };
+        }
+        
+            componentDidMount() {
+                axios
+                    .get("https://api.github.com/users/Riley-Robinson")
+                        .then(res => {
+                            console.log(res.data.followers_url);
+                                this.setState({
+                                    user: res.data
+                                });
+                        })
 
+                        .catch(err => console.log(err));
+
+            axios
+                .get("https://api.github.com/users/Riley-Robinson/followers")
+                        .then(res => {
+                            console.log(res.data);
+                            this.setState({
+                                followers: res.data
+                            });
+                        })
+                    .catch(err => console.log(err));
+            }
+
+
+            render() {
+                return (
+                    <div className="cards">
+                        <div className="main-card">
+                        {
+                            <UserCard
+                                login={this.state.user.login}
+                                id={this.state.user.id}
+                                avatar_url={this.state.user.avatar_url}
+                                location={this.state.user.location}
+                                html_url={this.state.user.html_url}
+                            />    
+                        }
+                        </div>
+
+                        <div className="folloers-card">
+                         {this.state.followers.map(follower => (
+                            <UserCard
+                                login={follower.login}
+                                id={follower.id}
+                                avatar_url={follower.avatar_url}
+                                location={follower.location}
+                                html_url={follower.html_url}
+                                                                />   
+                             ))}
+                        </div>
+                    </div>
+                );
+            }
+    }
 
 
 
